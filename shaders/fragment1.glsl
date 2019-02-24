2c3bfa2942fcd39111604a49d9db97c94bad0b26
Alexey-Ershov <ershov_ae@mail.ru>
2019-02-24 19:34:44 +0300

    Some code changes

--- shaders/fragment1.glsl
+++ shaders/fragment1.glsl
@@ -32,69 +32,15 @@ struct Primitive
 uniform Primitive objects[1] = Primitive[1](
         Primitive(float3(0.0, 0.0, -4.0), 0.05, SPHERE));
 
-float3 EyeRayDir(float x, float y, float w, float h)
-{
-	float fov = 3.141592654f/(2.0f); 
-    float3 ray_dir;
-  
-	ray_dir.x = x+0.5f - (w/2.0f);
-	ray_dir.y = y+0.5f - (h/2.0f);
-	ray_dir.z = -(w)/tan(fov/2.0f);
-	
-  return normalize(ray_dir);
-}
-
-bool RayBoxIntersection(float3 ray_pos, float3 ray_dir, float3 boxMin, float3 boxMax, inout float tmin, inout float tmax)
-{
-  ray_dir.x = 1.0f/ray_dir.x;
-  ray_dir.y = 1.0f/ray_dir.y;
-  ray_dir.z = 1.0f/ray_dir.z; 
-
-  float lo = ray_dir.x*(boxMin.x - ray_pos.x);
-  float hi = ray_dir.x*(boxMax.x - ray_pos.x);
-  
-  tmin = min(lo, hi);
-  tmax = max(lo, hi);
-
-  float lo1 = ray_dir.y*(boxMin.y - ray_pos.y);
-  float hi1 = ray_dir.y*(boxMax.y - ray_pos.y);
-
-  tmin = max(tmin, min(lo1, hi1));
-  tmax = min(tmax, max(lo1, hi1));
-
-  float lo2 = ray_dir.z*(boxMin.z - ray_pos.z);
-  float hi2 = ray_dir.z*(boxMax.z - ray_pos.z);
-
-  tmin = max(tmin, min(lo2, hi2));
-  tmax = min(tmax, max(lo2, hi2));
-  
-  return (tmin <= tmax) && (tmax > 0.f);
-}
-
-
-float3 RayMarchConstantFog(float tmin, float tmax, inout float alpha)
-{
-  float dt = 0.05f;
-	float t  = tmin;
-	
-	alpha  = 1.0f;
-	float3 color = float3(0,0,0);
-	
-	while(t < tmax && alpha > 0.01f)
-	{
-	  float a = 0.05f;
-	  color += a*alpha*float3(1.0f,1.0f,0.0f);
-	  alpha *= (1.0f-a);
-	  t += dt;
-	}
-	
-	return color;
-}
-
 const int MAX_MARCHING_STEPS = 255;
 const float MIN_DIST = 0.0;
 const float MAX_DIST = 100.0;
 const float EPSILON = 0.0001;
+
+struct Light {
+    float3 position;
+    float intensity;
+};
 
 float sphereSDF(float3 samplePoint, float radius)
 {
@@ -137,12 +83,24 @@ bool ray_intersect(const float3 orig, const float3 dir)
   }
 }
 
-float3 cast_ray(const float3 orig, const float3 dir) {
+float4 cast_ray(const float3 orig, const float3 dir) {
     if (!ray_intersect(orig, dir)) {
-        return float3(0.2, 0.7, 0.8); // background color
+        return float4(0.2, 0.7, 0.8, 1.0); // background color
     }
 
-    return float3(0.4, 0.4, 0.3);
+    return float4(0.4, 0.4, 0.3, 1.0);
+}
+
+float3 EyeRayDir(float x, float y, float w, float h)
+{
+    float fov = 3.141592654f/(2.0f); 
+    float3 ray_dir;
+  
+    ray_dir.x = x+0.5f - (w/2.0f);
+    ray_dir.y = y+0.5f - (h/2.0f);
+    ray_dir.z = -(w)/tan(fov/2.0f);
+    
+  return normalize(ray_dir);
 }
 
 void main(void)
@@ -171,7 +129,5 @@ void main(void)
   float tmin = 1e38f;
   float tmax = 0;
 
-  fragColor = float4(cast_ray(ray_pos, ray_dir), 1);
+  fragColor = cast_ray(ray_pos, ray_dir);
 }
-
-
